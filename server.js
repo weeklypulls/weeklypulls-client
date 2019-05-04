9712fe241c1a8ff8a40a63519156db551e3c6f05
Robert Kuykendall <robert@rkuykendall.com>
2019-05-04 13:52:28 -0400

    Try to get Heroku working

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-/* eslint-disable no-magic-numbers, no-console */
-
-var path = require('path');
-var express = require('express');
-
-var app = express();
-
-const dist = path.join(__dirname, 'dist');
-app.use(express.static(dist));
-app.set('port', process.env.PORT || 8080);
-app.get('*', (req, res) => res.sendFile(path.resolve(dist, 'index.html')));
-
-var server = app.listen(app.get('port'),() => {
-  console.log('listening on port ', server.address().port);
+const path = require('path');
+const express = require('express');
+const favicon = require('express-favicon');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
