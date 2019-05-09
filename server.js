cbc5002c687e081e2125fb97c42edcd004082111
Robert Kuykendall <robert@rkuykendall.com>
2019-05-09 08:26:47 -0400

    Test favicon fix

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/', function (req, res) {
+
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
