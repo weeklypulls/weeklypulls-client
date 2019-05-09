73734e78b4b33d7c7aae6466d237919942b19e72
Robert Kuykendall <robert@rkuykendall.com>
2019-05-09 08:42:37 -0400

    Test favicon fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
+// The __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
